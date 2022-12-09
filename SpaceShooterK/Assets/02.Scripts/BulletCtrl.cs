619a6cea0f15712050180024519f96c5b507cf4d
Karin-Me <asdotong@gmail.com>
2022-12-09 16:30:23 +0900

    20221209-02
    절대강좌 유니티
    05_총 발사 로직 에서
    204 페이지
    총알 스크립트 작성 전까지

---
+++ SpaceShooterK/Assets/02.Scripts/BulletCtrl.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCtrl : MonoBehaviour
+{
+    // ÃÑ¾ËÀÇ ÆÄ±«·Â
+    public float damage = 20.0f;
+    // ÃÑ¾ËÀÇ ¹ß»ç Èû
+    public float force = 1500f;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        
+    }
+}
--- SpaceShooterK/Assets/02.Scripts/FollowCam.cs
+++ SpaceShooterK/Assets/02.Scripts/FollowCam.cs
@@ -17,11 +17,16 @@ public class FollowCam : MonoBehaviour
     [Range(0.0f, 10.0f)]
     public float height = 2.0f;
 
+    // X 축으로 이동할 높이
+    [Range(-10f, 10.0f)]
+    public float width = 0f;
+
     // 반응 속도
     public float damping = 10.0f;
 
     // 카메라 LookAt 의 Offset 값
-    public float targetOffset = 2.0f;   //Target Offset 적용
+    public float targetOffsetX = 2.0f;   //Target Offset 적용
+    public float targetOffsetY = 0f;
 
     // SmoothDamp 에서 사용할 변수
     private Vector3 velocity = Vector3.zero;
@@ -38,24 +43,46 @@ public class FollowCam : MonoBehaviour
     {
         // ====01_로직부터.==== //     
 
-
-
-        // 03_로직_SmoothDamp 로직.        
+        // 02_로직_Slerp 로직.
 
         // 추적해야 할 대상의 뒤쪽으로 distance 만큼 이동
         // 높이를 height 만큼 이동
-        Vector3 pos = targetTr.position + (-targetTr.forward * distance) + (Vector3.up * height);
+        // 너비를 width 만큼 이동
 
-        // SmoothDamp 을(를) 이용한 위치 보간.
+        Vector3 pos = targetTr.position + (-targetTr.forward * distance) + (Vector3.up * height) + (Vector3.right * width);
 
-        // camTr.position 은 시작 위치
-        // pos 은 목표 위치
-        // ref velocity(current velocity) 은 현재 속도
-        // damping 은 목표 위치까지 도달할 시간
-        camTr.position = Vector3.SmoothDamp(camTr.position, pos, ref velocity, damping);
+        //Vector3 pos = targetTr.position + (-targetTr.forward * distance) + (Vector3.up * height) +
+        //                                  (-targetTr.right * distance) + (Vector3.right * width);
+
+        // 구면 선형 보간 함수를 사용해 부드럽게 위치를 변경
+        // camTr.position 은(는) 시작위치
+        // pos 은(는) 목표 위치
+        // Time.deltaTime * damping 은(는) 시간 t
+        camTr.position = Vector3.Slerp(camTr.position, pos, Time.deltaTime * damping);   
 
         // Camera 를 피벗 좌표를 향해 회전 (Target Offset 적용)
-        camTr.LookAt(targetTr.position + (targetTr.up * targetOffset));
+        camTr.LookAt(targetTr.position + (targetTr.up * targetOffsetX) + (targetTr.right * targetOffsetY));
+
+
+
+
+        //// 03_로직_SmoothDamp 로직.        
+
+        //// 추적해야 할 대상의 뒤쪽으로 distance 만큼 이동
+        //// 높이를 height 만큼 이동
+        //// 너비를 width 만큼 이동
+        //Vector3 pos = targetTr.position + (-targetTr.forward * distance) + (Vector3.up * height) + (Vector3.right * width);
+
+        //// SmoothDamp 을(를) 이용한 위치 보간.
+
+        //// camTr.position 은 시작 위치
+        //// pos 은 목표 위치
+        //// ref velocity(current velocity) 은 현재 속도
+        //// damping 은 목표 위치까지 도달할 시간
+        //camTr.position = Vector3.SmoothDamp(camTr.position, pos, ref velocity, damping);
+
+        //// Camera 를 피벗 좌표를 향해 회전 (Target Offset 적용)
+        //camTr.LookAt(targetTr.position + (targetTr.up * targetOffsetX) + (targetTr.right * targetOffsetY));
     }
 }
 
